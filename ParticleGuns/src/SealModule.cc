526f9c892a9b7a5eb3b03540aa97b61ad73b29d9
Jia Fu Low <jia.fu.low@cern.ch>
2021-06-14 20:11:29 -0400

    Create ParticleGuns 1/3

---
+++ ParticleGuns/src/SealModule.cc
@@ -0,0 +1,8 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "L1TMuonSimulations/ParticleGuns/interface/FlatRandomPtGunProducer2.h"
+#include "L1TMuonSimulations/ParticleGuns/interface/FlatEvtVtxGenerator2.h"
+
+using edm::FlatRandomPtGunProducer2;
+DEFINE_FWK_MODULE(FlatRandomPtGunProducer2);
+DEFINE_FWK_MODULE(FlatEvtVtxGenerator2);
