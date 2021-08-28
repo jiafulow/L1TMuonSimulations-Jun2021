c95b51e9c8d67dd80850ab114022fa453c30c923
Jia Fu Low <jia.fu.low@cern.ch>
2021-08-28 15:17:40 -0400

    Drop L1TrackTrigger_step and L1simulation_step from ntupler when running on PU200 samples

--- Configuration/python/customise.py
+++ Configuration/python/customise.py
@@ -13,12 +13,24 @@ def customise_ntupler_step(process):
     process.TFileService = cms.Service('TFileService', fileName = process.ntupler.fileName)
     process.ntuplerSequence = cms.Sequence(process.ntupler)
     try:
-      process.ntupler_step = cms.Path(process.generator+process.phase2L1EMTFSequence+process.ntuplerSequence)
+        process.ntupler_step = cms.Path(process.generator+process.phase2L1EMTFSequence+process.ntuplerSequence)
     except AttributeError:
-      process.ntupler_step = cms.Path(process.phase2L1EMTFSequence+process.ntuplerSequence)
+        process.ntupler_step = cms.Path(process.phase2L1EMTFSequence+process.ntuplerSequence)
     process.schedule.extend([process.ntupler_step])
 
     # Remove cms.EndPath instances from schedule
     paths_in_schedule = [path for path in process.schedule if not isinstance(path, cms.EndPath)]
     process.schedule = cms.Schedule(*paths_in_schedule)
+    return process
+
+def remove_L1TrackTrigger_step(process):
+    # Remove L1TrackTrigger_step from schedule
+    if process.L1TrackTrigger_step in process.schedule:
+        process.schedule.remove(process.L1TrackTrigger_step)
+    return process
+
+def remove_L1simulation_step(process):
+    # Remove L1simulation_step from schedule
+    if process.L1simulation_step in process.schedule:
+        process.schedule.remove(process.L1simulation_step)
     return process
